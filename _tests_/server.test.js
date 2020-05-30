66c0a11cb3e543b86b4df4b7a0bdea7d06c56994
dirrbick <kory.a.jaxn@gmail.com>
2020-05-30 14:18:03 -0700

    got a test to pass, refactored server.js

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -7,3 +7,24 @@
 // test if user already exists???
 // 
 // test that it is encrypted
+const serverObj = require('../lib/server.js');
+const supergoose = require('@code-fellows/supergoose');
+
+const mockRequest = supergoose(serverObj.server);
+
+describe('happy path', () => {
+  it('can create a user', async () => {
+    let response = await mockRequest.post('/v1/api/signup').send({
+      username: 'kJackson',
+      password: 'kPass',
+    });
+
+    expect(response.status).toBe(201);
+
+    expect(response.body._id).toBeDefined();
+    expect(response.body.password).toBeUndefined();
+    expect(response.body.password).not.toBe('kPass');
+  });
+
+
+});
